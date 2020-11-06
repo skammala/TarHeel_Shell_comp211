6ccfcfd9d398764578fc35210205f9b2587b2566
Shiva Kammala <shiva1@live.unc.edu>
2020-11-06 21:26:55 +0000

    finished main.c

--- src/main.c
+++ src/main.c
@@ -8,55 +8,28 @@
 #define MAX_CHARS 30
 
 char *string_calloc(size_t);
-char *string_realloc(char*, size_t);
 void print_str_contents(Str*);
 
 int main()
 {
-    char *line = string_calloc(MAX_CHARS);
-    char *first_idx = line;
-    size_t capacity = MAX_CHARS;
+    char *line = malloc(MAX_CHARS);
+    Str c = Str_value(MAX_CHARS);
+
     while((line = fgets(line, MAX_CHARS, stdin)) != NULL) {
+        Str_append(&c, line);
+
         if(strchr(line, '\n') != NULL) {
-            line = first_idx;
-            Str c = Str_value(20);
-            Str_append(&c, line);
             print_str_contents(&c);
             Str_drop(&c);
-            free(line);
-            line = string_calloc(MAX_CHARS);
-            first_idx = line;
-        } else {
-            line = first_idx;
-            string_realloc(line, (capacity *= 2));
-            line = strchr(line, '\0');
+            c = Str_value(MAX_CHARS);
         }
+
     }
     free(line);
-    first_idx = NULL;
+    Str_drop(&c);
     return EXIT_SUCCESS;
 }
 
-char *string_calloc(size_t nitems) {
-    char *string = calloc(nitems, sizeof(char));
-    if(string == NULL) {
-        fprintf(stderr, "Out of memory");
-        exit(EXIT_FAILURE);
-    }
-    return string;
-}
-
-char *string_realloc(char *string, size_t new_capacity) {
-    string = realloc(string, new_capacity * sizeof(char));
-    if(string == NULL) {
-        fprintf(stderr, "Out of memory");
-        exit(EXIT_FAILURE);
-    }
-    return string;
-}
-
 void print_str_contents(Str* string) {
-    for(size_t i = 0; i < Str_length(string); ++i) {
-        printf("%c", Str_get(string, i));
-    }
+    printf("%s", Str_cstr(string));
 }
